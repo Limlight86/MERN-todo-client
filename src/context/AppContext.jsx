a288d17a2f9b7e14c993d618bc192c6fcbe4145c
Lazaro <lquin008@gmail.com>
2020-04-08 18:28:21 -0400

    context for tasks endpoint get in place

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,14 +1,23 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState, useEffect, useCallback } from "react";
+import axios from 'axios'
 
 const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
   const [user, setUser] = useState({})
   const [loggedIn, setLoggedIn] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+  const token = localStorage.getItem("token")
+
+  const fetchTasks = useCallback(() => {
+    axios.get(`http://localhost:8080/tasks`,{headers: {Authorization: `Bearer ${token}`}})
+    .then(({ data }) => {
+      console.log(data)
+      setTasks(data)
+    })}, [token])
 
   useEffect(()=>{
-    const token = localStorage.getItem("token")
-
     if (token){
       fetch("http://localhost:8080/users/me", {
         headers: {
@@ -19,10 +28,16 @@ const AppContextProvider = ({ children }) => {
       .then(res => {
         setUser(res)
         setLoggedIn(true)
-    })}}, [])
+    })}}, [token])
+
+    useEffect(()=>{
+      if(token){
+        fetchTasks()
+      }
+    }, [loggedIn, fetchTasks, token])
 
   return (
-    <AppContext.Provider value={{user, setUser, loggedIn, setLoggedIn}}>
+    <AppContext.Provider value={{user, setUser, loggedIn, setLoggedIn, tasks}}>
       {children}
     </AppContext.Provider>
   );
