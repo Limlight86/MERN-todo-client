9bd8fd48b5587fb5115f171268609ca5e3e3e409
Lazaro <lquin008@gmail.com>
2020-05-21 16:37:41 -0400

    updated Home page to conditionally show todos based on loggedIn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,25 @@
-import React from "react"
+import React, {useContext} from "react"
+import { AppContext } from '../context/AppContext';
 import DateFilters from "../components/DateFilters"
 import TaskForm from "../components/TaskForm"
 import TasksContainer from "../components/TasksContainer"
 
 const Home = () => {
+  const { loggedIn } = useContext(AppContext)
 
   return(
     <div>
       <DateFilters />
-      <TaskForm />
-      <TasksContainer />
+      {
+        loggedIn ? ( 
+        <div>
+          <TaskForm />
+          <TasksContainer />
+        </div>) 
+        :
+        <h1>Log in to manage your Todos!</h1>
+      }
+      
     </div>
   )
 }
