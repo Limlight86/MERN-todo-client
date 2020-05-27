c0dd43600dc03e7b4b6abf91b20998fa5f96532a
Lazaro <lquin008@gmail.com>
2020-05-27 11:21:37 -0400

    redid history imports to components themselves

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useContext } from 'react';
-import {Link} from "react-router-dom"
+import {Link, useHistory} from "react-router-dom"
 import { AppContext } from "../context/AppContext";
 
 const Signup = () => {
-  const { user, setUser, setLoggedIn, history } = useContext(AppContext);
+  const { setUser, setLoggedIn } = useContext(AppContext);
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [name, setName] = useState("")
+
+  const history = useHistory()
 
   const signUp = (email, password, name, e) => {
     e.preventDefault()
@@ -32,43 +34,51 @@ const Signup = () => {
       setPassword("")
       setName("")
       localStorage.setItem("token", res.token );
-      history.push("/home");
+      history.push("/");
       })
-      .catch(() => console.log('error'))
+      .catch((e) => console.log(e.message))
   }
 
   return(
     <div>
       <form onSubmit={e => signUp(email, password, name, e)}>
           <h1>Sign Up Below!</h1>
-          <input
-            type="email"
-            name="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={ e => setEmail(e.target.value) }
-            required
-          />
-          <input
-            type="password"
-            name="password"
-            placeholder="Enter password"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            required
-          />
-          <input
-            type="name"
-            name="name"
-            placeholder="Enter name"
-            value={name}
-            onChange={e => setName(e.target.value)}
-            required
-          />
-          <input type="submit" value="Sign Up"/>
-          {
-          user.name && <h1>{user.name}</h1>
-        }
+          <div className="form-group">
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter email"
+              value={email}
+              onChange={ e => setEmail(e.target.value) }
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Password: </label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="name">Name: </label>
+            <input
+              type="name"
+              name="name"
+              id="name"
+              placeholder="Enter name"
+              value={name}
+              onChange={e => setName(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className="btn btn-primary actions">Sign Up</button>
         </form>
         <Link to="/">
           Return to Home Page
